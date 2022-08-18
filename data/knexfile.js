172612c3ad90a5b1fbeee47ba0d426471095d5e3
Anton Jaldegren <anton.jaldegren@gmail.com>
2022-08-18 16:56:40 +0200

    Added postgresql for staging/production

--- data/knexfile.js
+++ data/knexfile.js
@@ -17,4 +17,26 @@ module.exports = {
 			afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
 		},
 	},
+	staging: {
+		client: "postgresql",
+		connection: process.env.DATABASE_URL,
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "knex_migrations",
+		},
+	},
+	production: {
+		client: "postgresql",
+		connection: process.env.DATABASE_URL,
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "knex_migrations",
+		},
+	},
 };

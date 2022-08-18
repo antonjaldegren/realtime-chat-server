c1a63c7ccd41981550f54af3af24cb1d03407e62
Anton Jaldegren <anton.jaldegren@gmail.com>
2022-08-18 17:09:35 +0200

    Changed data type of room_id

--- data/migrations/20220622140729_init.js
+++ data/migrations/20220622140729_init.js
@@ -13,7 +13,7 @@ exports.up = async function (knex) {
 		table.string("message").notNullable();
 		table.string("author_id").notNullable();
 		table.string("author_username").notNullable();
-		table.string("room_id").notNullable();
+		table.integer("room_id").notNullable();
 		table.integer("created_at");
 
 		table.foreign("room_id").references("rooms.id").onDelete("CASCADE");
